6acfbf27ec00102632c5b2f0247fc94ca9ff8ff6
Naranmandakh <eco8xt@gmail.com>
2016-05-06 21:38:05 +0900

    Revert "нүүр хуудас"
    This reverts commit d5b979739e05f1fd735c4fc37c8d4106e1bbef9f.

--- com.bob-system.elements/footer/footer.php
+++
@@ -1,7 +0,0 @@
-<html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="com.bob-system.elements/footer.css">
-	</head>
-	<body></body>
-	<div class="fback"><pre>CopyRight 2016</pre></div>
-</html>
